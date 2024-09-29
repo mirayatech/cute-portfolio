3f5f25b02721b90167b7e4f74b9ebf2b405b2b2c
mirayatech <mirayaabrodi@gmail.com>
2024-09-29 08:40:39 +0200

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-[rgb(251,250,255)]`}>
+        {children}
+      </body>
     </html>
   );
 }
